bc6a5ff850f86f377d4b749fe08a21a28fd5d175
Will Scott <scottiewe@github.com>
2022-09-30 18:18:06 -0400

    Deployed storage and function
    removed unnecessary files

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = "https://getresumecounter4.azurewebsites.net/api/GetResumeCounter?code=A3CaeDy_D16riRn9afc07Gf45N2zNXNL4ZL3cMHhuMItAzFuX2oilg==";
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.")
